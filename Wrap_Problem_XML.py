4a965f6ac2e6d15426a11c6b07c43250d335c8a9
Colin-Fredericks <colin.fredericks@gmail.com>
2017-03-22 15:24:50 -0400

    It's working! Run to see instructions.

--- Wrap_Problem_XML.py
+++ Wrap_Problem_XML.py
@@ -5,24 +5,49 @@ import HTMLParser
 import sys
 import os
 
-# Get directory from command line argument
 
+instructions = """
+To use:
+Put all your problem XML files into a single folder.
+Run this on that folder: python Wrap_Problem_XML.py foldername
+Take the resulting all_the_problems.xml file
+Put it into the 'conditional' folder in your course, replacing the one that's already there.
+Rezip and upload the course.
+"""
+
+
+# Get directory from command line argument
 try:
     directory = sys.argv[1]
 except IndexError:
     # No directory listed
-    sys.exit('no directory listed')
+    sys.exit(instructions)
 
 # Open the selected folder and make a list of all the problems' filenames
 all_files = os.listdir(directory)
 
-# Make a "vertical" tag as the top of an XML tree
-vertical_tag = ET.Element('problem')
-vertical_tag.set('display_name', 'Every Single Autorandom Problem')
-vertical_tree = ET.ElementTree(vertical_tag)
+# Make a "conditional" tag as the top level
+# https://edx.readthedocs.io/projects/edx-open-learning-xml/en/latest/problem-xml/conditional_module.html
+conditional_tag = ET.Element('conditional')
+conditional_tag.set('sources', 'i4x://HarvardX/QMB1/problem/88aa6c33d4ab46139177fb1b56237a2b')
+conditional_tag.set('correct', 'True')
+conditional_tag.set('message', 'This space intentionally left blank.')
 
+# If there are any characters in the filename that would normally get escaped, we'll unescape them.
+parser = HTMLParser.HTMLParser()
 
-# Add a "conditional" tag inside the vertical
-#   https://edx.readthedocs.io/projects/edx-open-learning-xml/en/latest/problem-xml/conditional_module.html
 # Make a "problem" tag for each problem inside the conditional, with url_name equal to filename
-# Output the vertical XML file in the parent folder for the selected one
+for filename in all_files:
+    if filename[0] is not '.':    # Skipping hidden files
+        filename = filename.replace('.xml','')
+        problem_tag = ET.SubElement(conditional_tag, 'problem')
+        problem_tag.set('url_name', filename)
+
+# Output the conditional XML file in the parent folder for the selected one.
+xml_tree = ET.ElementTree(conditional_tag)
+xml_string = minidom.parseString(ET.tostring(xml_tree.getroot())).toprettyxml(indent="    ")
+xml_string = parser.unescape(xml_string)
+with open('all_the_problems.xml', "w") as f:
+    # We start from character 23 because the XML declaration is an unwanted 22 characters (counting \r).
+    # I should do this better, but this works for now.
+    f.writelines(xml_string[23:])
