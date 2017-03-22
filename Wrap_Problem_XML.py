ca085833ffaacad2ae3879494ea53452ce7755f7
Colin-Fredericks <colin.fredericks@gmail.com>
2017-03-22 12:07:43 -0400

    pseudocode

---
+++ Wrap_Problem_XML.py
@@ -0,0 +1,6 @@
+# Open the selected folder
+# Make a list of all the problems' filenames
+# Make a "vertical" tag as the top of an XML tree
+# Give it a display_name
+# Make a "problem" tag for each problem, with url_name equal to filename
+# Output the XML file in the parent folder for the selected one
