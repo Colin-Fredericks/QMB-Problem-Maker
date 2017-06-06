6cdc0ee8f729ab487f03ea0d26372d294cd6a6da
kclem <kclem@users.noreply.github.com>
2017-06-06 11:36:49 -0400

    Update QMB_utils.py
    Add load_matlab_matrix to load custom (static) matlab matrices

--- QMB_utils.py
+++ QMB_utils.py
@@ -1,3 +1,4 @@
+import os.path
 def html_table(listOfLists, formatting=[]):
   retStr = "<table>"
   for i in range(len(listOfLists)):
@@ -13,12 +14,32 @@ def html_table(listOfLists, formatting=[]):
   retStr = retStr + '</table>'
   return retStr
 
-def html_table2(listOfLists, formatting=[]):
-  retStr = "<table>"
-  for sublist in listOfLists:
-    retStr = retStr+ '  <tr><td>[</td>'
-    for x in sublist:
-        retStr += '<td>'+str(x)+'</td>'
-    retStr = retStr + "<td>]</td></tr>\n"
-  retStr = retStr + '</table>'
+def html_table_matlab(listOfLists, formatting=[]):
+  retStr = "myArray=[<table>"
+  for i in range(len(listOfLists)):
+    retStr = retStr+ '  <tr><td></td>'
+    for j in range(len(listOfLists[0])):
+        formatStart = ""
+        formatEnd = ""
+        if formatting and formatting[i][j]:
+            formatStart = "<"+formatting[i][j]+">"
+            formatEnd = "</"+formatting[i][j]+">"
+        retStr += '<td>'+formatStart+str(listOfLists[i][j])+formatEnd+'</td>'
+    retStr = retStr + "<td></td></tr>\n"
+  retStr = retStr + '</table>];'
   return retStr
+
+def load_matlab_matrix(matrixName):
+    #dlmwrite('myArray3.txt',myArray3)
+    #pathToFiles = 'C:/Users/Kendell/Documents/MATLAB/'
+    pathToFiles = 'matlabArrays/'
+    fileSuffix = ".txt"
+    fileLoc = pathToFiles+matrixName+fileSuffix
+    print('opening '+fileLoc)
+    if (not os.path.isfile(fileLoc)):
+        return ""
+
+    ins = open(fileLoc,"r")
+    data = [[int(n) for n in line.split(",")] for line in ins]
+    return data
+    
