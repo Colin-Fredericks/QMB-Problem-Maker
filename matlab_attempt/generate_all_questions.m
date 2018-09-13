a2239b33ef81333f2860ae1586f2e265537e31b8
Brian Buechel <buechelbrian+home@gmail.com>
2018-09-13 00:37:22 -0400

    Updated trees

--- matlab_attempt/generate_all_questions.m
+++ matlab_attempt/generate_all_questions.m
@@ -2,16 +2,19 @@
 clear
 close all
 
-addpath('Problem functions')
+addpath('util')
+file = 'Problem descriptions\Loop questions.xlsx';
 
-fpath = 'C:\Users\brian\Google Drive\Quant Methods for Biology\Adaptive\Sections\3. Arrays\';
-fname = [fpath 'Array questions.xlsx'];
+
 [~,sheets] = xlsfinfo(fname);
 sheets = sheets(strncmp(sheets,'CG',2));
 
 num_dynamic = 30;
 
-for ii = [14 17 18]%1:length(sheets)
+for ii = 1:length(sheets)
     parse_excel(fname,'sheet',sheets{ii},'num_dynamic',num_dynamic);
     fprintf('Finished sheet %s (%d of %d)\n',sheets{ii},ii,length(sheets))
 end
+
+% Use matlab to run the the batch file for the python code
+!make_xml
