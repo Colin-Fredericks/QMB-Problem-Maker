334b1d6e379fd4142f70080b6ed80d1a4c64e468
Brian Buechel <buechelbrian+home@gmail.com>
2018-09-13 20:01:55 -0400

    Ficed generate all questions to use the new format

--- matlab_attempt/generate_all_questions.m
+++ matlab_attempt/generate_all_questions.m
@@ -1,17 +1,19 @@
-%Generate all array questions
+%Generate all questions
 clear
 close all
 
-addpath('Problem functions')
+addpath('util')
+file = 'Problem descriptions\Basics questions.xlsx';
 
-fpath = 'C:\Users\brian\Google Drive\Quant Methods for Biology\Adaptive\Sections\3. Arrays\';
-fname = [fpath 'Array questions.xlsx'];
-[~,sheets] = xlsfinfo(fname);
+[~,sheets] = xlsfinfo(file);
 sheets = sheets(strncmp(sheets,'CG',2));
 
-num_dynamic = 30;
+num_dynamic = 1;
 
-for ii = [14 17 18]%1:length(sheets)
+for ii = 1:length(sheets)
     parse_excel(fname,'sheet',sheets{ii},'num_dynamic',num_dynamic);
     fprintf('Finished sheet %s (%d of %d)\n',sheets{ii},ii,length(sheets))
 end
+
+% Use matlab to run the batch file that calls the python script 
+!make_xml
