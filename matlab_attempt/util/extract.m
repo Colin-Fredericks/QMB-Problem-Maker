2e0e5c3cf0721a5662be4a9fdeaf29b82b2faa2b
Brian Buechel <buechelbrian+home@gmail.com>
2018-09-13 21:08:41 -0400

    Added N-dimensional functionality to extract()

--- matlab_attempt/util/extract.m
+++ matlab_attempt/util/extract.m
@@ -1,13 +1,22 @@
-function values = extract(array,indices)
+function values = extract(array,varargin)
 % function EXTRACT - Helper function for writing the QMB problems in excel
 %
 %   values = extract(array,indices) is equivalent to:
 %   values = array(indices);
 %
-%   I know this is stupid. This is helpful so I can use this in the Excel
-%   document that defines QMB problems
+%   values = extract(array,row_indices,col_indices) is equivalent to:
+%   values = array(row_indices,col_indices). 
 %
-%   Normally, I'd have to assign something to a variable using CODE, but
-%   this will allow me to skip that step.
+%   values = extract(array,dim1_ind,dim2_ind,dim3_ind, ...) will extract
+%   values according to each dimension.
+%
+%
 
-values = array(indices);
+if nargin == 2
+    values = array(varargin{1});
+elseif nargin > 2
+    string_args = cellfun(@mat2string,varargin,'UniformOutput',false);
+    values = eval(['array(' strjoin(string_args,',') ');']);
+else
+    error('Extract must have 2 or more input arguments');
+end
